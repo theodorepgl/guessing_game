3060c43cb77dbc87c8e9a9c46f265e5c280fd39c
theodorepgl <limlimtheo96@gmail.com>
2023-08-15 15:36:12 +0800

    first commit

---
+++ guess.rb
@@ -0,0 +1,25 @@
+secret_number = rand(1..100)
+puts secret_number
+
+counter = 1
+chances = 6
+guesses = []
+
+while counter <= chances 
+	puts 'Choose a number between 1 to 100'
+	answer = gets.to_i
+	puts answer
+
+	if answer > secret_number
+		puts "TOO HIGH"
+	elsif answer < secret_number
+		puts "TOO LOW"
+	else 
+		break puts "Your number is correct"
+	end
+
+	guesses << answer
+	puts "guesses: #{answer}"
+	
+	counter += 1
+end
