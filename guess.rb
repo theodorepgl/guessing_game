f5a58c280d45be2886e05facb2da95ead6a71c8d
theodorepgl <limlimtheo96@gmail.com>
2023-08-16 12:36:16 +0800

    standings list

--- guess.rb
+++ guess.rb
@@ -1,25 +1,44 @@
-secret_number = rand(1..100)
-puts secret_number
+scoring_card = {}
 
-counter = 1
-chances = 6
-guesses = []
+loop do
+	puts "Hello, what's your name?"
+	player_name = gets.chomp
+	puts "hi #{player_name}"
 
-while counter <= chances 
-	puts 'Choose a number between 1 to 100'
-	answer = gets.to_i
-	puts answer
+	secret_number = rand(1..100)
+	puts secret_number
 
-	if answer > secret_number
-		puts "TOO HIGH again"
-	elsif answer < secret_number
-		puts "TOO LOW again"
-	else 
-		break puts "Your number is correct"
+	counter = 1
+	chances = 6
+	guesses = []
+
+	while tries <= chances 
+		puts 'Choose a number between 1 to 100'
+		answer = gets.to_i
+		puts "Your guesses: #{answer}"
+		guesses << answer
+		print guesses
+
+		if answer > secret_number
+			puts "TOO HIGH again"
+		elsif answer < secret_number
+			puts "TOO LOW again"
+		else 
+			break puts "Your number is correct"
+		end
+		counter += 1
 	end
 
-	guesses << answer
-	puts "guesses: #{answer}"
-	
-	counter += 1
+	puts "STANDINGS"
+	scoring_card [player_name] = counter
+	scoring_card.sort_by {|player,score| score}.each do |player, score|
+		puts "#{player}: #{score}"
+	end
+
+	puts 'Do you want to play again?'
+	puts 'Y - Yes'
+	puts 'N - No'
+
+	play = gets.chomp.downcase
+	break if play != 'y'
 end
